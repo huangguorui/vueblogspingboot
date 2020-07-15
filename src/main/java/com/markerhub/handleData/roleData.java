55147177bf17330a46f06d25dede2c0805c6c3ab
huangguorui <2315684336@qq.com>
2020-07-15 22:51:31 +0800

    支持role列表二层循环

---
+++ src/main/java/com/markerhub/handleData/roleData.java
@@ -0,0 +1,19 @@
+package com.markerhub.mapper.handleData;
+
+
+import com.markerhub.entity.Permission;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+@Data
+public class roleData implements Serializable {
+
+        private Integer id;
+
+        private String roleName;
+
+        private String roleDesc;
+
+       public List<Permission> permissions;
+    }
